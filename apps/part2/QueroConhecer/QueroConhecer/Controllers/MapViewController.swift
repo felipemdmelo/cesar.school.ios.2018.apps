f04e2658f23ee88449b66a472200c83e9897f726
Douglas Frari <douglas.frari@gmail.com>
2018-06-01 23:56:25 -0300

    Atualizando projeto

--- apps/part2/QueroConhecer/QueroConhecer/Controllers/MapViewController.swift
+++ apps/part2/QueroConhecer/QueroConhecer/Controllers/MapViewController.swift
@@ -22,11 +22,45 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        for place in places {
-            print(place.name)
+        
+        // esconder os componentes
+        searchBar.isHidden = true
+        viInfo.isHidden = true
+        
+        // para permitir alterar a annotation mostrada na view do mapa
+        mapView.delegate = self
+        
+        // configura o título
+        if places.count == 1 {
+            title = places[0].name
+        } else {
+            title = "Meus lugares"
         }
         
+        for place in places {
+            addMap(place)
+        }
+        
+        showPlaces()
+    }
+    
+    func addMap(_ place: Place) {
+        print("Place -> \(place.name)")
+        places.append(place)
+        
+        // usando Annotations
+        //let annotation = MKPointAnnotation()
+        
+        // usando PlaceAnnotation do nosso model
+        let annotation = PlaceAnnotation(coordinate: place.coordinate, type: .place)
+        annotation.address = place.address
+        annotation.coordinate = place.coordinate // variavel computada
+        annotation.title = place.name
+        mapView.addAnnotation(annotation)
+    }
+    
+    func showPlaces() {
+        mapView.showAnnotations(mapView.annotations, animated: true)
     }
     
      
@@ -36,7 +70,39 @@ class MapViewController: UIViewController {
     
     @IBAction func showSearchBar(_ sender: UIBarButtonItem) {
     }
-    
-    
-    
+   
 }
+
+
+extension MapViewController: MKMapViewDelegate {
+    
+    // usado para recuperar uma annotationView para permitir modifica-la
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if !(annotation is PlaceAnnotation) {
+            // mantem o default caso nao seja o tipo esperado.
+            return nil
+        }
+        let placeAnnotation = annotation as! PlaceAnnotation
+        let type = placeAnnotation.type
+        let identifier = "\(type)"
+        // tentando reusar uma view do pino padrao (MKMarkerAnnotationView)
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
+        
+        if annotationView == nil {
+            // criando pela primeira vez uma annotation view
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        }
+        annotationView?.annotation = annotation
+        annotationView?.canShowCallout = true // informacoes extras na forma de um balao
+        // usando Point of Interess
+        annotationView?.markerTintColor = type == .place ? UIColor(named: "main") : UIColor(named: "poi")
+        annotationView?.glyphImage = type == .place ?  #imageLiteral(resourceName: "placeGlyph") :  #imageLiteral(resourceName: "poiGlyph")
+        annotationView?.displayPriority = type == .place ? .required : .defaultHigh
+        
+        return annotationView
+    }
+}
+
+
+
---
+++ apps/part2/QueroConhecer/QueroConhecer/Models/PlaceAnnotation.swift
@@ -0,0 +1,29 @@
+//
+//  PlaceAnnotation.swift
+//  QueroConhecer
+//
+//  Created by Douglas Frari on 01/06/18.
+//  Copyright © 2018 CESAR School. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class PlaceAnnotation: NSObject, MKAnnotation {
+    
+    enum PlaceType {
+        case place
+        case poi
+    }
+    
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    var type: PlaceType
+    var address: String?
+    
+    init(coordinate: CLLocationCoordinate2D, type: PlaceType) {
+        self.coordinate = coordinate
+        self.type = type
+    }
+}
