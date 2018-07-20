dce7124759bb64d63ee141acbddd98193b88c051
Douglas Frari <douglas_delfrari@apple.com>
2018-07-20 13:00:39 -0300

    Versão do projeto com a parte inicial - dia 20

---
+++ apps/20_21_julho/MyGames/MyGames/Controllers/GameViewController.swift
@@ -0,0 +1,41 @@
+//
+//  GameViewController.swift
+//  MyGames
+//
+//  Created by Douglas Frari on 20/07/18.
+//  Copyright © 2018 CESAR School. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbConsole: UILabel!
+    @IBOutlet weak var lbReleaseDate: UILabel!
+    @IBOutlet weak var ivCover: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
