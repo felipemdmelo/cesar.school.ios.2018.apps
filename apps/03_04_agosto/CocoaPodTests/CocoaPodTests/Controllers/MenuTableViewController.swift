8cab7341f748595fa36e16cb86599c98a689df11
Douglas Frari <douglas.frari@gmail.com>
2018-08-04 08:26:17 -0300

    Versao demo do Cocoapod (bug fix)

--- apps/03_04_agosto/CocoaPodTests/CocoaPodTests/Controllers/MenuTableViewController.swift
+++ apps/03_04_agosto/CocoaPodTests/CocoaPodTests/Controllers/MenuTableViewController.swift
@@ -13,15 +13,7 @@ class MenuTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Define the menus
-        SideMenuManager.default.menuLeftNavigationController = storyboard!.instantiateViewController(withIdentifier: "LeftMenuNavigationController") as? UISideMenuNavigationController
-        
-        
-        // Enable gestures. The left and/or right menus must be set up above for these to work.
-        // Note that these continue to work on the Navigation Controller independent of the View Controller it displays!
-        SideMenuManager.default.menuAddPanGestureToPresent(toView: self.navigationController!.navigationBar)
-        SideMenuManager.default.menuAddScreenEdgePanGesturesToPresent(toView: self.navigationController!.view)
+    
     }
 
 
